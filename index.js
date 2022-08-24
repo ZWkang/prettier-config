36299d0727fdd3b865054fa125d43c4597c944bc
zwkang <kang95630@gmail.com>
2022-08-24 21:27:13 +0800

    feat: update

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 module.exports = {
+  "$schema": "http://json.schemastore.org/prettierrc",
   printWidth: 120,
   tabWidth: 2,
   useTabs: false,
   semi: true,
   singleQuote: true,
   quoteProps: 'as-needed',
-  jsxSingleQuote: false,
   trailingComma: 'all',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
   arrowParens: 'always',
   rangeStart: 0,
   rangeEnd: Infinity,
@@ -16,6 +15,5 @@ module.exports = {
   insertPragma: false,
   htmlWhitespaceSensitivity: 'css',
   vueIndentScriptAndStyle: false,
-  endOfLine: 'lf',
-  operatorLineBreak: 'before',
+  endOfLine: 'lf'
 };
